f304e289d1f9ce287b401e7f642d45759a961540
Shug <shugsidhu1108@gmail.com>
2018-06-10 13:33:22 -0700

    checkpoint complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
-    @posts.each_with_index do |post, index|
-      if index % 5  == 0
-        post.title = "SPAM"
-      end
-    end
+  	@posts = Post.all
   end
 
   def show
@@ -13,25 +8,50 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+  	@post = Post.new
   end
 
   def create
-    @post = Post.New
+    @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
 
-    if @post.Save
-
+    if @post.save
       flash[:notice] = "Post was saved."
       redirect_to @post
     else
-
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
     end
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+
+    if @post.save
+      flash[:notice] = "Post was updated."
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    if @post.destroy
+      flash[:notice] = "Post was deleted."
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "There was an error deleting the post. Please try again."
+      render :show
+    end
+
   end
 end
