8c6523e2dade61901e01f59441339c211afd532a
Shug <shugsidhu1108@gmail.com>
2018-06-10 21:53:22 -0700

    assignment comp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 resources :posts
+resources :questions
+
 get 'about' => 'welcome#about'
-  get 'welcome/faq'
-  root 'welcome#index'
+
+  root to: 'welcome#index'
 end
