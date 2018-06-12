7e481f450449f76e42b91e13df12cfeae84cbfcc
Shug <shugsidhu1108@gmail.com>
2018-06-12 12:47:37 -0700

    checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_160542) do
+ActiveRecord::Schema.define(version: 2018_06_11_182246) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 2018_05_29_160542) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "topic_id"
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.boolean "public", default: true
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
