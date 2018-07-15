5a6ee0d6e9584dd4e204138f9fcdb691b4b0a5db
Shug <shugsidhu1108@gmail.com>
2018-07-15 08:53:33 -0700

    error

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,24 +1,22 @@
 class QuestionsController < ApplicationController
   def index
-  	@questions = Question.all
+    @questions = Question.all
   end
 
   def show
-  	@question = Question.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def new
-  	@question = Question.new
+    @question = Question.new
   end
 
   def create
-  	@question = Question.new
+    @question = Question.new
     @question.title = params[:question][:title]
     @question.body = params[:question][:body]
-    @question.resolved = false
-
     if @question.save
-      flash[:notice] = "Question was saved."
+      flash[:notice] = "Question was asked!"
       redirect_to @question
     else
       flash.now[:alert] = "There was an error saving the question. Please try again."
@@ -27,32 +25,31 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-  	@question = Question.find(params[:id])
+    @question = Question.find(params[:id])
   end
 
   def update
-  	@question = Question.find(params[:id])
-  	@question.title = params[:question][:title]
-  	@question.body = params[:question][:body]
-  	@question.resolved = params[:question][:resolved]
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
 
-  	if @question.save
-  	  flash[:notice] = "Post was updated."
-  	  redirect_to @question
-  	else
-  	  flash.now[:alert] = "There was an error saving the question. Please try again."
-  	  render :edit
-  	end
+    if @question.save
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :edit
+    end
   end
 
   def destroy
-  	@question = Question.find(params[:id])
-  	if @question.destroy
-  	  flash[:notice] = "Question was deleted."
-  	  redirect_to questions_path
-  	else
-  	  flash.now[:alert] = "There was an error deleting the question. Please try again."
-  	  render :show
-  	end
+    @question = Question.find(params[:id])
+    if @question.destroy
+      flash[:notice] = "\"#{@question.title}\" was deleted successfully."
+      redirect_to questions_path
+    else
+      flash.now[:alert] = "There was an error deleting the question."
+      render :show
+    end
   end
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
-     
-     # Should test for name
+
+     it { is_expected.to have_many(:posts) }
+
+          # Should test for name
      it { is_expected.to validate_presence_of(:name) }
      it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
