bfbcdc44f2c5e2a00dfc1cf81fde107fbce21f80
Shug <shugsidhu1108@gmail.com>
2018-07-29 15:33:02 -0700

    assignment done

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,39 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  #creates new instance of a post class, computes and stores given values
+  let(:name) { RandomData.random_sentence }
+   let(:description) { RandomData.random_paragraph }
+   let(:title) { RandomData.random_sentence }
+   let(:body) { RandomData.random_paragraph }
+ # #3
+   let(:topic) { Topic.create!(name: name, description: description) }
+ # #4
+   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
+   let(:post) { topic.posts.create!(title: title, body: body, user: user) }
 
-	let(:name) { RandomData.random_sentence }
-	let(:description) { RandomData.random_paragraph }
-	let(:title) { RandomData.random_sentence }
-	let(:body) { RandomData.random_paragraph }
-	let(:topic) { Topic.create!(name: name, description: description) }
+   it { is_expected.to have_many(:comments) }
+   it { is_expected.to have_many(:votes) }
+   it { is_expected.to belong_to(:topic) }
+   it { is_expected.to belong_to(:user) }
 
-	let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
- # #2
-  let(:post) { topic.posts.create!(title: title, body: body, user: user) }
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+   it { is_expected.to validate_presence_of(:user) }
 
-	it { is_expected.to have_many(:comments) }
-	it { is_expected.to have_many(:votes) }
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
-	it { is_expected.to belong_to(:topic) }
-	it { is_expected.to belong_to(:user) }
+  describe "attributes" do
+    it "has title and body attributes" do
+      expect(post).to have_attributes(title: title, body: body, user: user)
+    end
+  end
 
-	it { is_expected.to validate_presence_of(:title) }
-	it { is_expected.to validate_presence_of(:body) }
-	it { is_expected.to validate_presence_of(:topic) }
-	it { is_expected.to validate_presence_of(:user) }
-
-	it { is_expected.to validate_length_of(:title).is_at_least(5) }
-	it { is_expected.to validate_length_of(:body).is_at_least(20) }
-
-	describe "attributes" do
-		it "has a title, body, and user attribute" do
-       expect(post).to have_attributes(title: title, body: body, user: user)
-		end
-	end
-
-	describe "voting" do
- # #5
+  describe "voting" do
      before do
        3.times { post.votes.create!(value: 1, user: user) }
        2.times { post.votes.create!(value: -1, user: user) }
@@ -41,45 +39,57 @@ RSpec.describe Post, type: :model do
        @down_votes = post.votes.where(value: -1).count
      end
 
- # #6
      describe "#up_votes" do
        it "counts the number of votes with value = 1" do
          expect( post.up_votes ).to eq(@up_votes)
        end
      end
 
- # #7
      describe "#down_votes" do
        it "counts the number of votes with value = -1" do
          expect( post.down_votes ).to eq(@down_votes)
        end
      end
 
- # #8
      describe "#points" do
        it "returns the sum of all down and up votes" do
          expect( post.points ).to eq(@up_votes - @down_votes)
        end
      end
-		 describe "#update_rank" do
-		  # #28
-		        it "calculates the correct rank" do
-		          post.update_rank
-		          expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
-		        end
 
-		        it "updates the rank when an up vote is created" do
-		          old_rank = post.rank
-		          post.votes.create!(value: 1, user: user)
-		          expect(post.rank).to eq (old_rank + 1)
-		        end
+     describe "#update_rank" do
+       it "calculates the correct rank" do
+         post.update_rank
+         expect(post.rank).to eq (post.points + (post.created_at - Time.new(1970,1,1)) / 1.day.seconds)
+       end
 
-		        it "updates the rank when a down vote is created" do
-		          old_rank = post.rank
-		          post.votes.create!(value: -1, user: user)
-		          expect(post.rank).to eq (old_rank - 1)
-		        end
-		      end
+       it "updates the rank when an up vote is created" do
+         old_rank = post.rank
+         post.votes.create!(value: 1, user: user)
+         expect(post.rank).to eq (old_rank + 1)
+       end
 
+       it "updates the rank when a down vote is created" do
+         old_rank = post.rank
+         post.votes.create!(value: -1, user: user)
+         expect(post.rank).to eq (old_rank - 1)
+       end
+     end
+
+     describe "#create_vote" do
+       it "sets the post up_votes to 1" do
+         expect(post.up_votes).to eq(1)
+       end
+
+       it "calls #create_vote when a post is created" do
+         post = topic.posts.new(title: RandomData.random_sentence, body:RandomData.random_sentence, user: user)
+         expect(post).to receive(:create_vote)
+         post.save
+       end
+
+       it "associates the vote with the owner of the post" do
+         expect(post.votes.first.user).to eq(post.user)
+       end
+     end
    end
 end
