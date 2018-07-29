d17bfe484a77ceb13c37bd4749a448248203e623
Shug <shugsidhu1108@gmail.com>
2018-07-29 14:30:28 -0700

    checkpoint done

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,12 +26,16 @@ end
 topics = Topic.all
 
 50.times do
-	Post.create!(
+	post = Post.create!(
 		user:   users.sample,
 		topic:  topics.sample,
 		title:  RandomData.random_sentence,
 		body:   RandomData.random_paragraph
 	)
+# #12
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ # #13
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 posts = Post.all
 
@@ -63,4 +67,8 @@ puts "Seed finished"
 puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
+
+
 puts "#{Comment.count} comments created"
